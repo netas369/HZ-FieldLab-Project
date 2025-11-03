9eda456d1917eef979d59a2b407039bc2fbeaac8
Netas Neverauskas <neve0023@hz.nl>
2025-11-03 14:34:20 +0100

    made a navbar with 2 buttons history and live

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <navbar />
+
     <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/NavBar.vue";
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {Navbar}
 }
 </script>
 
